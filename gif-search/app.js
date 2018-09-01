28b1906bc3562455e2d6ca64bdcb1ccd32cd1e9c
Akhtar Zaman <auz.zamy@gmail.com>
2018-08-31 19:35:45 -0700

    Adding a get function

--- gif-search/app.js
+++ gif-search/app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.get ('/hello-world', function ( req, res) {
+    res.send('Hello World');
+});
+
+
+
 app.listen(3000, function () {
-    console.log('Gif Search Listening on port localhost:3000!');
+    console.log('Example app listening on port 3000!');
     });
